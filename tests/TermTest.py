77fd4f81f0fecb7f7757a464ea3d0b9e921ebba2
sumanta23 <nandisumanta0@gmail.com>
2015-11-28 20:58:29 +0530

    integation with Certificate

--- tests/TermTest.py
+++ tests/TermTest.py
@@ -1,18 +1,45 @@
 from pyscript import pyscript
 import io
+import unittest
+import unicodedata
 
-session = pyscript.open("https://localhost:8443")
-#response = session.terminal().execute("list rootca")
-response = session.terminal().execute("export -cat keystore rootca -serialno 1448654747524 -filename d.p12 -format p12");
+class TermTest(unittest.TestCase):
+    def exe(self,command):
+        session = pyscript.open("https://localhost:8443")
+
+        response = session.terminal().execute(command);
+
+        if type(response) is tuple:
+            filename = response[0]
+            content = response[1]
+            with io.open(filename, 'wb') as handle:
+                handle.write(content)
+        else:
+            line = response.get_output()
+            print line
+            return line
+
+        pyscript.close(session)
+
+    def fetchSerialNo(self,res):
+        serialno = res
+        s = serialno.split('\t')
+        s=s[2]
+        return s
+
+    def test_commands(self):
+        rstr = '<serialno>'
+        serialno = None
+        fd = open("commands",'rb')
+        for line in fd.readlines():
+            if rstr in line:
+                line = line.replace(rstr,serialno)
+            print 'executing: ' + line
+            res = self.exe(line)
+            if res is not None and len(res) > 6:
+                res = unicodedata.normalize('NFKD',res).encode('ascii','ignore')
+                serialno = self.fetchSerialNo(res)
 
 
-if type(response[1]) is bytearray:
-    filename = response[0]
-    content = response[1]
-    with io.open(filename, 'wb') as handle:
-        handle.write(content)
-else:
-    line = response.get_output()
-    print line
-
-pyscript.close(session)
+if __name__ == '__main__':
+    unittest.main()
